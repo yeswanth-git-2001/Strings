383d5088c9736d9777d0f3f82ddaa16023ea7ffe
yeswanth <yeswanthchunchu@gmail.com>
2024-05-08 17:52:38 +0530

    main.java file update

--- main.java
+++ main.java
@@ -2,5 +2,14 @@ class Main {
     public static void main(String[] args) {
         StringBasics stringBasics = new StringBasics();
         stringBasics.display();
+
+        demo demo = new demo();
+        System.out.println(demo); //When ever you called the reference, toString() will be invoked
+        System.out.println(demo.toString());
     }
+}
+
+class demo{
+    String s = "Yeswanth";
+    int id = 10;
 }
